a168de59688a201a230008d5da8b5f72a40e2d0b
Aadarsh Senapati <57413192+aadarshsenapati@users.noreply.github.com>
2024-07-14 02:05:45 +0530

    Add files via upload

---
+++ Birthday.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	int x;
+	Beep(264, 250);
+	Sleep(500);
+	Beep(264, 250);
+	Sleep(250);
+	Beep(297, 1000);
+	Sleep(250);
+	Beep(264, 1000);
+	Sleep(250);
+	Beep(352, 1000);
+	Sleep(250);
+	Beep(330, 2000);
+	Sleep(500);
+	Beep(264, 250);
+	Sleep(500);
+	Beep(264, 250);
+	Sleep(250);
+	Beep(297, 1000);
+	Sleep(250);
+	Beep(264, 1000);
+	Sleep(250);
+	Beep(396, 1000);
+	Sleep(250);
+	Beep(352, 2000);
+	Sleep(500);
+	Beep(264, 250);
+	Sleep(500);
+	Beep(264, 250);
+	Sleep(250);
+	Beep(264, 1000);
+	Sleep(250);
+	Beep(440, 1000);
+	Sleep(250);
+	Beep(352, 500);
+	Sleep(250);
+	Beep(352, 250);
+	Sleep(250);
+	Beep(330, 1000);
+	Sleep(250);
+	Beep(297, 2000);
+	Sleep(500);
+	Beep(466, 250);
+	Sleep(500);
+	Beep(466, 250);
+	Sleep(250);
+	Beep(440, 1000);
+	Sleep(250);
+	Beep(352, 1000);
+	Sleep(250);
+	Beep(396, 1000);
+	Sleep(250);
+	Beep(352, 2000);// FREQUENCY, TIME
+	Sleep(250);// DELAY
+}
